92d73138c9bfaf17c939f2124559d5d96a74f99d
Joe Kaufeld <joe.kaufeld@gmail.com>
2019-02-22 13:22:20 -0500

    add postgres db
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- filamentcolors/settings/base.py
+++ filamentcolors/settings/base.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -84,8 +87,12 @@ WSGI_APPLICATION = 'filamentcolors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'filamentcolors',
+        'USER': 'filamentcolors',
+        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

a3a0701d44b86f4c068eb9db0c56cf77af0e7e6c
Joe Kaufeld <joe.kaufeld@gmail.com>
2019-03-02 00:36:20 -0500

    add twitter support
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- filamentcolors/settings/base.py
+++ filamentcolors/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ssssh I\'m hunting wabbits')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "ssssh I'm hunting wabbits")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ filamentcolors/twitter_helpers.py
@@ -0,0 +1,57 @@
+import os
+import random
+
+from dotenv import load_dotenv
+
+from twitter import Api
+
+load_dotenv()
+
+api = Api(
+    consumer_key=os.environ.get('TWITTER_CONSUMER_KEY'),
+    consumer_secret=os.environ.get('TWITTER_CONSUMER_SECRET'),
+    access_token_key=os.environ.get('TWITTER_ACCESS_TOKEN_KEY'),
+    access_token_secret=os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+)
+
+intro_phrases = [
+    'Swatcheriffic!',
+    'Just added a new swatch!',
+    "What's that I see? A new swatch‽",
+    "New swatch ahoy!",
+    "A new swatch appears!",
+    "More filament!",
+    "Another swatch? Another!",
+    "Let there be color!",
+    "What's this?",
+    "More plastic has arrived!",
+    "Where'd this come from?",
+    "A new swatch!",
+]
+
+outro_phrases = [
+    'can be found here:',
+    'can be seen here:',
+    'joins the library!',
+    'now available!',
+    'is now available!',
+    'now listed!',
+    'has been added!'
+    'has been added here:'
+    'now listed!',
+    'is now listed!',
+    'is now in the library!',
+    'can be found in the library!',
+    'appears here:',
+    'is listed here:',
+]
+
+
+def send_tweet(swatch):
+    import ipdb; ipdb.set_trace()
+    plural = "'" if swatch.manufacturer.endswith("s") else "'s"
+    api.PostUpdate(
+        f'{random.choice(intro_phrases)} {swatch.manufacturer}{plural} {swatch.color_name}'
+        f' {swatch.filament_type} {random.choice(outro_phrases)}'
+        f' https://filamentcolors.xyz/swatch/{swatch.id}'
+    )

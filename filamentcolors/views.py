8cdf5d880bf1b80094efd2b284150b43b0e8ffd3
Joe Kaufeld <joe.kaufeld@gmail.com>
2020-07-26 15:31:13 -0400

    streamline welcome experience trigger
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- filamentcolors/views.py
+++ filamentcolors/views.py
@@ -10,8 +10,7 @@ from filamentcolors.helpers import (
     get_custom_library,
     get_hsv,
     get_swatches,
-    set_tasty_cookies,
-    show_welcome_modal,
+    prep_request
 )
 from filamentcolors.models import GenericFilamentType, Swatch, Post
 
@@ -60,14 +59,7 @@ def librarysort(request, method: str = None):
 
     data.update({"swatches": items})
 
-    if show_welcome_modal(request):
-        data.update({"launch_welcome_modal": True})
-        # https://stackoverflow.com/a/38798861
-        response = render(None, html, data)
-        set_tasty_cookies(response)
-        return response
-
-    return render(request, html, data)
+    return prep_request(request, html, data)
 
 
 def colorfamilysort(request, family_id):
@@ -80,13 +72,7 @@ def colorfamilysort(request, family_id):
 
     data.update({"swatches": s})
 
-    if show_welcome_modal(request):
-        data.update({"launch_welcome_modal": True})
-        response = render(None, html, data)
-        set_tasty_cookies(response)
-        return response
-
-    return render(request, html, data)
+    return prep_request(request, html, data)
 
 
 def manufacturersort(request, id):
@@ -104,13 +90,7 @@ def manufacturersort(request, id):
 
     data.update({"swatches": s})
 
-    if show_welcome_modal(request):
-        data.update({"launch_welcome_modal": True})
-        response = render(None, html, data)
-        set_tasty_cookies(response)
-        return response
-
-    return render(request, html, data)
+    return prep_request(request, html, data)
 
 
 def typesort(request, id):
@@ -128,13 +108,7 @@ def typesort(request, id):
 
     data.update({"swatches": s})
 
-    if show_welcome_modal(request):
-        data.update({"launch_welcome_modal": True})
-        response = render(None, html, data)
-        set_tasty_cookies(response)
-        return response
-
-    return render(request, html, data)
+    return prep_request(request, html, data)
 
 
 def swatch_detail(request, id):
@@ -145,20 +119,13 @@ def swatch_detail(request, id):
     if not swatch:
         raise Http404
     else:
-
         swatch.refresh_cache_if_needed()
         if generate_custom_library(data):
             swatch.update_all_color_matches(get_custom_library(data))
 
         data.update({"swatch": swatch})
 
-        if show_welcome_modal(request):
-            data.update({"launch_welcome_modal": True})
-            response = render(None, html, data)
-            set_tasty_cookies(response)
-            return response
-
-        return render(request, html, data)
+        return prep_request(request, html, data)
 
 
 def swatch_collection(request, ids):
@@ -190,7 +157,7 @@ def swatch_collection(request, ids):
         }
     )
 
-    return render(request, "library.html", data)
+    return prep_request(request, "library.html", data)
 
 
 def edit_swatch_collection(request, ids):
@@ -203,13 +170,7 @@ def edit_swatch_collection(request, ids):
         {"swatches": get_swatches(data).order_by("-date_added"),}
     )
 
-    if show_welcome_modal(request):
-        data.update({"launch_welcome_modal": True})
-        response = render(None, html, data)
-        set_tasty_cookies(response)
-        return response
-
-    return render(request, html, data)
+    return prep_request(request, html, data)
 
 
 def inventory_page(request):
@@ -217,7 +178,7 @@ def inventory_page(request):
     data.update({
         "swatches": Swatch.objects.all(),
     })
-    return render(request, "inventory.html", data)
+    return prep_request(request, "inventory.html", data)
 
 
 def post_list(request):
@@ -225,7 +186,7 @@ def post_list(request):
     data.update({
         "posts": Post.objects.filter(published=True),
     })
-    return render(request, "post_list.html", data)
+    return prep_request(request, "post_list.html", data)
 
 
 def post_detail(request, post_id):
@@ -234,7 +195,7 @@ def post_detail(request, post_id):
         raise Http404
     data = build_data_dict(request)
     data.update({"post": post})
-    return render(request, "post_detail.html", data)
+    return prep_request(request, "post_detail.html", data)
 
 
 def post_preview(request, post_id):
@@ -243,20 +204,20 @@ def post_preview(request, post_id):
         raise Http404
     data = build_data_dict(request)
     data.update({"post": post})
-    return render(request, "post_detail.html", data)
+    return prep_request(request, "post_detail.html", data)
 
 
 def about_page(request):
-    return render(request, "about.html", build_data_dict(request))
+    return prep_request(request, "about.html", build_data_dict(request))
 
 
 def donation_page(request):
-    return render(request, "donations.html", build_data_dict(request))
+    return prep_request(request, "donations.html", build_data_dict(request))
 
 
 def error_404(request, *args, **kwargs):
-    return render(request, "404.html", build_data_dict(request), status=404)
+    return prep_request(request, "404.html", build_data_dict(request), status=404)
 
 
 def error_500(request, *args, **kwargs):
-    return render(request, "500.html", build_data_dict(request), status=500)
+    return prep_request(request, "500.html", build_data_dict(request), status=500)

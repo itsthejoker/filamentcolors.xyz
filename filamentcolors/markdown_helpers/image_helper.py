76a678caf54966466bf98ec170b4724676132211
Joe Kaufeld <joe.kaufeld@gmail.com>
2020-08-01 17:34:10 -0400

    fix post image display
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- filamentcolors/markdown_helpers/image_helper.py
+++ filamentcolors/markdown_helpers/image_helper.py
@@ -1,4 +1,6 @@
 import markdown
+from markdown.util import AtomicString
+from xml.etree.ElementTree import Element
 
 IMAGE_RE = r'!\[(.*)]\((.*)\)'
 
@@ -21,20 +23,46 @@ class ImageHelperPattern(markdown.inlinepatterns.Pattern):
         filename = m.group(2)
         url = m.group(3)
 
-        container = markdown.util.etree.Element("div")
-        container.set("class", "text-center photobox")
+        container = Element("div")
+        container.set("class", "container")
 
-        outsidelink = markdown.util.etree.Element("a")
+        row = Element("div")
+        row.set("class", "row")
+
+        filler_column = Element("div")
+        filler_column.set("class", "col-sm-0 col-md-2")
+
+        picture_column = Element("div")
+        picture_column.set("class", "col-sm-0 col-md-8")
+
+        picture_container = Element("div")
+        picture_container.set("class", "text-center")
+
+        outsidelink = Element("a")
         outsidelink.set("href", url)
         outsidelink.set("data-caption", filename)
 
-        img = markdown.util.etree.Element("img")
-        img.set("class", "img-fluid rounded")
+        img = Element("img")
+        img.set("class", "img-fluid border rounded postImg")
         img.set("src", url)
         img.set("alt", filename)
 
-        container.append(outsidelink)
+        clickme_container = Element("div")
+        clickme = Element("small")
+        clickme.text = AtomicString("Click for full-sized image")
+        clickme.set("class", "text-muted")
+        clickme_container.append(clickme)
+
         outsidelink.append(img)
+        outsidelink.append(clickme_container)
+        picture_container.append(outsidelink)
+        picture_column.append(picture_container)
+
+        container.append(row)
+        row.append(filler_column)
+        row.append(picture_column)
+        row.append(filler_column)
+
         return container
 
 

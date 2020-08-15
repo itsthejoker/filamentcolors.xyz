54b0447b584fd5aaad848fba466527e9e380218d
Joe Kaufeld <joe@kenzie.academy>
2020-08-15 19:29:31 -0400

    finish email magic!
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- filamentcolors/appstatic/js/email_magic.js
+++ filamentcolors/appstatic/js/email_magic.js
@@ -1,20 +1,36 @@
-function getBootstrapModalContent(data) {
+function getBootstrapModalContent(id, emailAddress, subject, cc, bcc, body) {
     return `
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal fade" id="emailmagic-${id}" tabindex="-1" role="dialog" aria-labelledby="Select your preferred email provider!" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Open email in...</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
               <div class="modal-body">
-                ...
+                <a 
+                  href="https://mail.google.com/mail/?view=cm&fs=1&to=${emailAddress}&su=${subject}&cc=${cc}&bcc=${bcc}&body=${body}"
+                  class="btn btn-block btn-outline-danger"
+                  target="_blank"
+                >Gmail</a>
+                <a 
+                  href="https://outlook.office.com/owa/?path=/mail/action/compose&to=${emailAddress}&subject=${subject}&body=${body}"
+                  class="btn btn-block btn-outline-primary"
+                  target="_blank"
+                >Outlook</a>
+                <a
+                  href="https://compose.mail.yahoo.com/?to=${emailAddress}&subject=${subject}&cc=${cc}&bcc=${bcc}&body=${body}"
+                  class="btn btn-block btn-outline-success"
+                  target="_blank"
+                >Yahoo! Mail</a>
+                <a href="mailto:${emailAddress}" class="btn btn-block btn-outline-info" target="_blank">Default</a>
+                <hr/>
+                <button class="btn btn-block btn-outline-dark" onclick="copyToClipboard('${emailAddress}')">Copy to Clipboard</button>
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
               </div>
             </div>
           </div>
@@ -22,21 +38,61 @@ function getBootstrapModalContent(data) {
     `
 }
 
-function getOrCreateModalAnchor() {
-
+function copyToClipboard(val){
+    navigator.clipboard.writeText(val);
 }
 
 function createID() {
+    // https://stackoverflow.com/a/3242438
     return Math.random().toString(36).substr(2, 9)
 }
 
-function magic(e) {
+function magic(e, id) {
+    // Don't let the browser actually start the email opening process
     e.preventDefault();
+    // is this actually necessary? Not sure.
     e.stopPropagation();
-    console.log(e.target.href);
-    console.log("fuck");
     // todo: swap out for vanilla js with bootstrap 5
-    $('#patreonModal').modal()
+    // jquery is already here because this is for bootstrap and
+    // bootstrap 4 requires it.
+    $(`#emailmagic-${id}`).modal()
+}
+
+function parseMailto(href) {
+    let emailAddress,
+        subject = "",
+        cc = "",
+        bcc = "",
+        body = "";
+
+    // remove "mailto:"
+    cleanedData = href.slice(href.indexOf(":")+1);
+    if (cleanedData.indexOf("?") !== -1) {
+        let parts = cleanedData.split("?");
+        emailAddress = parts[0]
+
+        parts = parts[1].split("&")
+        parts.forEach(function (snippet) {
+            snippet = snippet.split("=")
+            switch (snippet[0]) {
+                case "cc":
+                    cc = snippet[1];
+                    break;
+                case "bcc":
+                    bcc = snippet[1];
+                    break;
+                case "subject":
+                    subject = snippet[1];
+                    break;
+                case "body":
+                    body = snippet[1];
+            }
+        });
+
+    } else {
+        emailAddress = cleanedData
+    }
+    return [emailAddress, subject, cc, bcc, body]
 }
 
 Array.from(
@@ -44,14 +100,14 @@ Array.from(
 ).forEach(
     function (el, index) {
         if (el.href.indexOf("mailto") !== -1) {
+            let id = createID();
+            let newModal = getBootstrapModalContent(id, ...parseMailto(el.href))
+            document.body.insertAdjacentHTML("beforeend", newModal)
             el.addEventListener(
                 'click', function (e) {
-                    magic(e)
+                    magic(e, id)
                 }
             );
-            // el.setAttribute("data-toggle", "modal");
-            // el.setAttribute("href", "#patreonModal");
-            // el.setAttribute("data-target", "#patreonModal")
         }
     }
 );

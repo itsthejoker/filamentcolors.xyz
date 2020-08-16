1a3f688d59c1f104b57419f753a855685bba4dce
Joe Kaufeld <joe@kenzie.academy>
2020-08-15 22:51:45 -0400

    update with version from github
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- filamentcolors/appstatic/js/email_magic.js
+++ filamentcolors/appstatic/js/email_magic.js
@@ -1,4 +1,27 @@
-function getBootstrapModalContent(id, emailAddress, subject, cc, bcc, body) {
+/**
+ * @license
+ * Copyright (c) Joe Kaufeld -- github.com/itsthejoker.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+function getModalContent(id, emailAddress, subject, cc, bcc, body) {
     return `
         <div class="modal fade" id="emailmagic-${id}" tabindex="-1" role="dialog" aria-labelledby="Select your preferred email provider!" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
@@ -100,8 +123,11 @@ Array.from(
 ).forEach(
     function (el, index) {
         if (el.href.indexOf("mailto") !== -1) {
+            // If it's a mailto link, break it apart, then generate a modal
+            // for each mailto link. Stick that modal at the bottom of the
+            // document and let it chill out until it's called for.
             let id = createID();
-            let newModal = getBootstrapModalContent(id, ...parseMailto(el.href))
+            let newModal = getModalContent(id, ...parseMailto(el.href))
             document.body.insertAdjacentHTML("beforeend", newModal)
             el.addEventListener(
                 'click', function (e) {

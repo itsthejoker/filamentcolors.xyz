5164a9655e11a60de770a48f8848b6f9902e8071
Joe Kaufeld <opensource@joekaufeld.com>
2024-01-20 13:34:58 -0500

    :construction: change cookie to have explicit samesite reference

--- filamentcolors/appstatic/js/cookies/cookie.js
+++ filamentcolors/appstatic/js/cookies/cookie.js
@@ -7,7 +7,7 @@ function createCookie(name,value,days) {
         var expires = "; expires="+date.toGMTString();
     }
     else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/;secure";
+    document.cookie = name+"="+value+expires+"; path=/;secure;SameSite=Strict";
 }
 
 function readCookie(name) {

8a682e47b753670b4fe5569dee064b01ad9d384b
Joe Kaufeld <opensource@joekaufeld.com>
2024-01-23 22:57:04 -0500

    :monocle_face: add bugsnag

--- filamentcolors/settings/prod.py
+++ filamentcolors/settings/prod.py
@@ -2,3 +2,10 @@
 from filamentcolors.settings.base import *
 
 ENVIRONMENT = "prod"
+ALLOWED_HOSTS = ["*"]
+BUGSNAG = {
+    "api_key": os.environ.get("BUGSNAG_KEY"),
+    "project_root": str(BASE_DIR),
+}
+
+MIDDLEWARE = ["bugsnag.django.middleware.BugsnagMiddleware"] + MIDDLEWARE

8719e1b9a14ee330e1ae17818e415ee4cd6fa0f5
Joe Kaufeld <opensource@joekaufeld.com>
2024-07-06 17:15:42 -0400

    :art: run black

--- filamentcolors/__init__.py
+++ filamentcolors/__init__.py
@@ -14,4 +14,6 @@ NAVBAR_MESSAGE: str | None = (
     " <a href='https://instagram.com/filamentcolors' class='text-white' target='_blank'>Find us @filamentcolors!</a>"
 )
 # an ID for the 'don't show' cookie. Unique per message.
-NAVBAR_MESSAGE_ID: str | None = md5(NAVBAR_MESSAGE.encode('utf-8')).hexdigest() if NAVBAR_MESSAGE else None
+NAVBAR_MESSAGE_ID: str | None = (
+    md5(NAVBAR_MESSAGE.encode("utf-8")).hexdigest() if NAVBAR_MESSAGE else None
+)

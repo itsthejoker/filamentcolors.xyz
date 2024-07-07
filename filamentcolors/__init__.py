2e72e220b56cb20a9efd074f36e299431e073eb0
Joe Kaufeld <opensource@joekaufeld.com>
2024-07-07 13:16:08 -0400

    :lipstick: remove message about instagram

--- filamentcolors/__init__.py
+++ filamentcolors/__init__.py
@@ -9,10 +9,7 @@
 from hashlib import md5
 
 # a string that will be displayed in full at the top of the site. Can contain html.
-NAVBAR_MESSAGE: str | None = (
-    "We're on Instagram! Behind-the-scenes shots of how the prints get made :)"
-    " <a href='https://instagram.com/filamentcolors' class='text-white' target='_blank'>Find us @filamentcolors!</a>"
-)
+NAVBAR_MESSAGE: str | None = None
 # an ID for the 'don't show' cookie. Unique per message.
 NAVBAR_MESSAGE_ID: str | None = (
     md5(NAVBAR_MESSAGE.encode("utf-8")).hexdigest() if NAVBAR_MESSAGE else None

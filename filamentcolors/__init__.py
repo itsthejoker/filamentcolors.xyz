81bc404e2dc586c5fd5996ef2eb569d259e10dab
Joe Kaufeld <opensource@joekaufeld.com>
2024-08-21 00:05:04 -0400

    💬 add announcement about funding

--- filamentcolors/__init__.py
+++ filamentcolors/__init__.py
@@ -9,7 +9,7 @@
 from hashlib import md5
 
 # a string that will be displayed in full at the top of the site. Can contain html.
-NAVBAR_MESSAGE: str | None = None
+NAVBAR_MESSAGE: str | None = "Hey there - please take a moment to read <a href='https://forum.filamentcolors.xyz/d/10-looking-forward-whats-next-funding' class='alert-link text-white'>our message about the future of the site here.</a>"
 # an ID for the 'don't show' cookie. Unique per message.
 NAVBAR_MESSAGE_ID: str | None = (
     md5(NAVBAR_MESSAGE.encode("utf-8")).hexdigest() if NAVBAR_MESSAGE else None

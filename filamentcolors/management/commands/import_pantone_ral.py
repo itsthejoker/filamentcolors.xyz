ca46569070e77cba7b610e751f968242f7f699db
Joe Kaufeld <opensource@joekaufeld.com>
2025-09-24 22:43:54 -0400

    ✨ calculate and store LAB values during pantone import

--- filamentcolors/management/commands/import_pantone_ral.py
+++ filamentcolors/management/commands/import_pantone_ral.py
@@ -1,6 +1,8 @@
 import os
 import sqlite3
 
+from colormath.color_conversions import convert_color
+from colormath.color_objects import LabColor, sRGBColor
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -10,6 +12,12 @@ from filamentcolors.models import RAL, Pantone, PantonePMS, Swatch
 
 class Command(BaseCommand):
     help = "Import information from pantone.db"
+
+    def rgb_to_lab(self, r: int, g: int, b: int):
+        color = convert_color(sRGBColor(r, g, b, is_upscaled=True), LabColor)
+        color.set_illuminant("d65")
+        color.set_observer("10")
+        return float(color.lab_l), float(color.lab_a), float(color.lab_b)
 
     def handle(self, *args, **options):
         if not os.path.exists(os.path.join(settings.BASE_DIR, "pantone.db")):
@@ -22,12 +30,16 @@ class Command(BaseCommand):
         if Pantone.objects.count() == 0:
             to_write = []
             for obj in cur.execute("select * from pantone").fetchall():
+                L, a, b = self.rgb_to_lab(obj[1], obj[2], obj[3])
                 to_write.append(
                     Pantone(
                         code=obj[0],
                         rgb_r=obj[1],
                         rgb_g=obj[2],
                         rgb_b=obj[3],
+                        lab_l=L,
+                        lab_a=a,
+                        lab_b=b,
                         hex_color=obj[4][1:],
                         name=obj[5],
                         category=obj[6],
@@ -45,12 +57,16 @@ class Command(BaseCommand):
         if RAL.objects.count() == 0:
             to_write = []
             for obj in cur.execute("select * from ral").fetchall():
+                L, a, b = self.rgb_to_lab(obj[1], obj[2], obj[3])
                 to_write.append(
                     RAL(
                         code=obj[0],
                         rgb_r=obj[1],
                         rgb_g=obj[2],
                         rgb_b=obj[3],
+                        lab_l=L,
+                        lab_a=a,
+                        lab_b=b,
                         hex_color=obj[4][1:],
                         name=obj[5],
                         category=obj[6],
@@ -70,8 +86,18 @@ class Command(BaseCommand):
             for obj in cur.execute("select * from pms").fetchall():
                 code, hex_code = obj
                 r, g, b = hex_to_rgb(hex_code)
+                L, a, b = self.rgb_to_lab(r, g, b)
                 to_write.append(
-                    PantonePMS(code=code, rgb_r=r, rgb_g=g, rgb_b=b, hex_color=hex_code)
+                    PantonePMS(
+                        code=code,
+                        rgb_r=r,
+                        rgb_g=g,
+                        rgb_b=b,
+                        lab_l=L,
+                        lab_a=a,
+                        lab_b=b,
+                        hex_color=hex_code
+                    )
                 )
             PantonePMS.objects.bulk_create(to_write)
             self.stdout.write(

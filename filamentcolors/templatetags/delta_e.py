f34978c94ee7be7a11335bdb3423f76c3d3310b3
Joe Kaufeld <opensource@joekaufeld.com>
2026-02-02 09:26:38 -0500

    🐛 fix accidental conversion error

--- filamentcolors/templatetags/delta_e.py
+++ filamentcolors/templatetags/delta_e.py
@@ -8,4 +8,6 @@ register = template.Library()
 # https://stackoverflow.com/a/9948180
 @register.simple_tag
 def delta_e(obj_1, obj_2):
+    if hasattr(obj_2, "lab_l") and obj_2.lab_l is not None:
+        return obj_1.get_distance_to((obj_2.lab_l, obj_2.lab_a, obj_2.lab_b))
     return obj_1.get_distance_to(hex_to_rgb(obj_2.hex_color))
